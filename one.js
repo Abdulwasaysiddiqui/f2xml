2cb4bcdc5d52544b11796ff319bae95df4cba825
abdulwasaysiddiqui <abdulwasaysiddiqui704@gmail.com>
2024-01-10 18:18:05 +0500

    Saved mee

---
+++ one.js
@@ -0,0 +1,20 @@
+var organization = [
+
+    {
+        "id" : 0,
+        "name" : "abc",
+        "designation" : "Manager",
+        "salary" : 50000,
+        "phone-no" : 426501
+    },
+
+    {
+        "id" : 1,
+        "name" : "xyz",
+        "designation" : "Cashier",
+        "salary" : 80000,
+        "phone-no" : 501426
+    },
+
+
+]
